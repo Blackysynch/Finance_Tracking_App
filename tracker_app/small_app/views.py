7c0d7a7bd25b5b01c9398591449a9891e5fb0c26
Nkinyampraises <nkinyampraisesncha@gmail.com>
2024-09-11 11:42:24 +0100

    login backend

--- tracker_app/small_app/views.py
+++ tracker_app/small_app/views.py
@@ -2,29 +2,51 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.urls import reverse
+import os
+
+
 # Create your views here.
 
 def landingPage(request):
     return render(request, 'landingpage.html')
 
-
 def loginPage(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')   
+        username = request.POST.get('Name')
+        password = request.POST.get('password')
 
 
-        user = authenticate(username=username, password=password)
+        print(f" {username}, {password}")
+
+        if not username or not password:
+            messages.error(request, 'username and password are required')
+            return render(request, 'login.html')
+
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            messages.error(request, 'Invalid username or password')
+            return render(request, 'login.html')
+
+        user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request,   
- user)
-            # Successful authentication
-            return redirect(reverse(profilePage))  # Replace with your desired success page
-        else:
-            # Authentication failed
-            return render(request, 'login.html', {'error_message': 'Invalid credentials'})
+            if user.is_active:
 
-    return render(request, 'login.html')
+                login(request, user)
+                messages.success(request, 'You have successfully logged in')
+                return redirect('profile-page')
+            else:
+                messages.error(request, 'Account is inactive')
+                return render(request, 'login.html')
+        else:
+
+            print(f"User is {user} does not exist")
+            messages.error(request, 'Invalid username or password')
+            return render(request, 'login.html')
+
+    return render(request,'login.html')
+
+
 
 
 def signUpPage(request):
