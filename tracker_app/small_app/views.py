3791d1d1636301921e58004028b9130549691b48
Kenmogne matchuekam sergine <kenmognematchuekam.serginebarthe@ictuniversity.edu.cm>
2024-09-11 10:53:24 +0100

    backend for profile page

--- tracker_app/small_app/views.py
+++ tracker_app/small_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password
-from .models import User
+from .models import User,UserDetails
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.http import JsonResponse
@@ -19,7 +19,66 @@ def signUpPage(request):
     return render(request, 'sign-up.html')
 
 
+ @login_required
 def profilePage(request):
+    if request.method == 'POST':
+        user = request.user.id
+        name = request.POST.get('name')
+        email = request.user.email #request.POST.get('email')
+        telephone = request.POST.get('tel')
+        location = request.POST.get('location')
+        country = request.POST.get('country')
+
+        budgetmonthly = request.POST.get('monthly-budget')
+        source_of_income = request.POST.get('income-source')
+        other_sources = request.POST.get('additional-income')
+
+        profile_picture = request.FILES.get('profile_picture')
+
+        if not name:
+            messages.error(request, 'a name  is required.')
+        if not location:
+            messages.error(request, 'a location  is required.')
+        if not email:
+            messages.error(request, 'a email  is required.')
+        if not telephone:
+            messages.error(request, 'a tel  is required.')
+        if not country:
+            messages.error(request, 'a country  is required.')
+
+        if budgetmonthly:
+            try:
+                budgetmonthly = float(budgetmonthly)
+                if budgetmonthly <= 0:
+                    messages.error(request, 'Budget must be a positive number.')
+            except ValueError:
+                messages.error(request, 'Invalid budget format.')
+
+        if not messages.get_messages(request):
+            # Get the existing UserDetail instance or create a new one
+            user_detail, created = UserDetail.objects.get_or_create(user_id=user)
+
+            # Update the existing UserDetail instance
+            if created:
+                messages.success(request, 'Your profile was created.')
+        
+            user_detail.name = name
+            user_detail.email = email
+            user_detail.telephone = telephone
+            user_detail.location = location
+            user_detail.country = country
+            user_detail.budgetmonthly = budgetmonthly
+            user_detail.source_of_income = source_of_income
+            user_detail.other_sources = other_sources
+
+            # Save the profile picture
+            if profile_picture:
+                user_detail.profile_picture = profile_picture
+            user_detail.save()
+
+
+            return render(request, 'profilepage.html')
+
     return render(request, 'profilepage.html')
 
 
